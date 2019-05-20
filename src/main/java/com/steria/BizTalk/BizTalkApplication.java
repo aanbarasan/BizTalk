66fbb3a819733c59f1c5ea6e0a68a0f1cb667982
nandalive <nandhakumar.bism@gmail.com>
2019-05-20 12:23:07 +0530

    War deploy code

--- src/main/java/com/steria/BizTalk/BizTalkApplication.java
+++ src/main/java/com/steria/BizTalk/BizTalkApplication.java
@@ -4,17 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.steria.BizTalk.RestContoller.ResourceController;
 
 @SpringBootApplication
-public class BizTalkApplication {
+public class BizTalkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BizTalkApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BizTalkApplication.class);
 	}
 }
